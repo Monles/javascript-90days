7bde18199b594d8b4ffea01491841681566e0967
monles <iceueb@gmail.com>
2022-06-15 08:31:22 +0100

    first commit

---
+++ day05/babel_test.js
@@ -0,0 +1,80 @@
+// // ** var */
+// const level = 100;
+// var testVar = false;
+
+// if (level > 90) {
+//   var testVar = true;
+//   console.log("inside", testVar);
+// }
+// console.log("outside", testVar);
+
+/**_____________________________________*/
+
+// **let */
+// const level = 100;
+// let testVar = false;
+
+// // if (level > 90) {
+//   let testVar = true;
+//   console.log("inside", testVar);
+// }
+// console.log("outside", testVar);
+
+/**_____________________________________*/
+
+// // ** const */
+// const player = "Bobcorn";
+// const level = 100;
+// let testVar = false;
+
+// if (level > 90) {
+//   let testVar = true;
+// }
+
+// player = "Newbie";
+
+/**_____________________________________*/
+
+// const player = "Bobcorn";
+// const level = 100;
+// let testVar = false;
+
+// if (level > 90) {
+//   let testVar = true;
+// }
+
+// const obj = {
+//   palyer: "bobcorn",
+//   level: 100,
+//   testVar: false,
+// };
+
+/**_____________________________________*/
+
+// const name = "john doe";
+
+// const obj = {
+//   [name]: "hello",
+//   ["full" + "Name"]: "hee hey",
+// };
+
+/**_____________________________________*/
+// const name = "Bobcorn";
+// const age = 24;
+// const pet = "puppy";
+
+// const greeting = `Hello, my name is ${name} and I am ${age} years old. I have a ${pet}`;
+
+/**_____________________________________*/
+
+// function greet(name = "", age = 30, pet = "cat") {
+//   return `Hello, my name is ${name} and I am ${age} years old. I have a ${pet}`;
+// }
+
+/**_____________________________________*/
+
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = (a, b) => a + b;
---
+++ day05/switch.js
@@ -0,0 +1,20 @@
+// function rpgGame(commands) {
+//   var events;
+//   switch (commands) {
+//     case "forward":
+//       break;
+//       events = "hit by apples";
+//     case "left":
+//       events = "hit on a wall";
+//       break;
+//     case "right":
+//       events = "a monster chases you";
+//       break;
+//     case "back":
+//       events = "a bunch of monsters chase you";
+//       break;
+//     default:
+//       events = "Please enter a valid direction";
+//   }
+//   return events;
+// }
